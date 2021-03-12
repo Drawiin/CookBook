1bffaaad92bea9fbdadbb35ee40424887c04cc69
vinicius santos <viniciussantos@jera.com.br>
2021-03-12 00:51:51 -0400

    okay

--- app/src/main/java/com/cookbook/network/RecipeService.kt
+++ app/src/main/java/com/cookbook/network/RecipeService.kt
@@ -1,11 +1,22 @@
 package com.cookbook.network
 
+import com.cookbook.network.model.RecipeNetworkEntity
+import com.cookbook.network.model.responses.RecipeSearchResponse
 import retrofit2.http.GET
 import retrofit2.http.Header
+import retrofit2.http.Query
 
 interface RecipeService {
     @GET("search")
     suspend fun search(
-        @Header("Authorization") token: String
-    )
+        @Header("Authorization") token: String,
+        @Query("page") page: Int,
+        @Query("query") query: String
+    ): RecipeSearchResponse
+
+    @GET("get")
+    suspend fun get(
+        @Header("Authorization") token: String,
+        @Query("id") id: Int,
+    ): RecipeNetworkEntity
 }
