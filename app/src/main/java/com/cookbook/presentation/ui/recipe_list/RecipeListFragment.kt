c7f4fa9434e633d03f9c0151f3210f82b6dd7afc
vinicius santos <viniciussantos@jera.com.br>
2021-03-13 02:01:30 -0400

    first requests network

--- app/src/main/java/com/cookbook/presentation/ui/recipe_list/RecipeListFragment.kt
+++ app/src/main/java/com/cookbook/presentation/ui/recipe_list/RecipeListFragment.kt
@@ -1,10 +1,10 @@
 package com.cookbook.presentation.ui.recipe_list
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Text
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
@@ -12,7 +12,6 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.TextUnit
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import com.cookbook.util.TAG
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -28,12 +27,15 @@ class RecipeListFragment : Fragment() {
         return ComposeView(requireContext()).apply {
             setContent {
                 val recipes = viewModel.recipes.value
+                LazyColumn {
+                    items(recipes) { recipe ->
+                        Text(
+                            text = recipe.title ?: "",
+                            style = TextStyle(fontSize = TextUnit.Sp(25), color = Color.White)
+                        )
+                    }
+                }
 
-                recipes.forEach { Log.d(TAG, it.title.toString()) }
-                Text(
-                    text = "hEY",
-                    style = TextStyle(fontSize = TextUnit.Sp(25), color = Color.White)
-                )
             }
         }
     }
